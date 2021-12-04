a89a3fdc1f545c83c0f973d3c508d5b3aae19e94
Priyamesh Gupta <priyamesh28@gmai.com>
2021-12-04 14:16:20 +0530

    functionality using async await

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 let joke = document.querySelector('#joke');
 let btn = document.querySelector('#btn');
 
-const generatejokes = () => {
-    fetch("https://icanhazdadjoke.com/slack")
-        .then((data) => data.json())
-        .then(jokedata => {
-            let joketext = jokedata.attachments[0].text;
-            joke.innerHTML = joketext
-        })
+// const generatejokes = () => {
+//     fetch("https://icanhazdadjoke.com/slack")
+//         .then((data) => data.json())
+//         .then(jokedata => {
+//             let joketext = jokedata.attachments[0].text;
+//             joke.innerHTML = joketext
+//         })
+// }
+
+const generatejokes = async () => {
+
+    let data = await fetch("https://icanhazdadjoke.com/slack");
+    let jokedata = await data.json();
+    let joketext = jokedata.attachments[0].text;
+    joke.innerHTML = joketext;
+
 }
 
 generatejokes();
